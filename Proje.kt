a4a6017b83beedd565a1312e62211ff53cdf9b91
Ceren Akman <42838160+akmancrn@users.noreply.github.com>
2020-01-15 13:42:14 +0300

    Create Proje.kt

---
+++ Proje.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+
+import java.io.PrintStream
+import java.util.Scanner
+import java.util.*
+
+fun main (args : Array<String>)
+{
+   var str: String = "ANDROID PROGRAMLAMA"
+
+print("KOTLİN İLE" + str.trim()+"ÖĞRENİYORUM")
+
+}
